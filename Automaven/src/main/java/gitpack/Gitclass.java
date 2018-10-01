24f594c3c796deaa3ace6e8c6f9be4d491dc83b9
shivanandchachadi <shivu.2007@gmail.com>
2018-10-01 13:00:29 +0530

    this is second commit.added system. out .print msg

--- Automaven/src/main/java/gitpack/Gitclass.java
+++ Automaven/src/main/java/gitpack/Gitclass.java
@@ -1,10 +1,27 @@
 package gitpack;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Gitclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver","c:\\Chromedriver\\chromedriver.exe");
+			
+			WebDriver driver=new ChromeDriver();
+			
+			driver.get("http://operauat.softvision.com/Account/Login");
+			driver.findElement(By.xpath(".//input[@id='UserName']")).sendKeys("name");
+			driver.findElement(By.xpath(".//input[@id='Password'][@name='Password']")).sendKeys("password");
+			driver.findElement(By.xpath("//input[@type='submit'][@value='Sign in']")).click();
+			System.out.println("this is to check edit,commit and push to remote repository");
+		}
 
-	}
+	//this is second comment
+	
 
 }
